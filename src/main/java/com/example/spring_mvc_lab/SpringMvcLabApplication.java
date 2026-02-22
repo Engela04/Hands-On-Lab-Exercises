c4bda16906c6a555715e99b0cf4c820ae5bc16f6
Engela04 <2482010@unai.edu>
2026-02-22 20:23:43 +0700

    menambahkan Handson

---
+++ src/main/java/com/example/spring_mvc_lab/SpringMvcLabApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring_mvc_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcLabApplication.class, args);
+	}
+}
